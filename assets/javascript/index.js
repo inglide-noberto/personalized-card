81f87c1a3bb2d8b9143b85e58438ea335e303bc1
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-21 18:42:03 -0300

    Construindo a funcionalidade do projeto
     Funções de mudança de valores

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,28 @@
-const numberCard = document.querySelector("#number");
+const numberInput = document.querySelector("#number");
+const nameInput = document.querySelector("#name");
+const cvcInput = document.querySelector("#cvc");
+const dateMInput = document.querySelector("#dateM");
+const dateYInput = document.querySelector("#dateY");
+const confirm = document.querySelector(".button-confirm");
+
+const numberCard = document.querySelector(".numberCard");
+const nameCard = document.querySelector(".nameCard");
+const date = document.querySelector(".dateCard");
+const cvc = document.querySelector(".cvcCard");
+
+nameInput.addEventListener('change', (e) => { 
+    e.preventDefault();
+    nameCard.textContent = e.target.value ;
+});
+numberInput.addEventListener('change', (e) => {
+    e.preventDefault();
+    numberCard.textContent = e.target.value
+});
+dateYInput.addEventListener('change', (e) => {
+    e.preventDefault(); 
+    date.textContent = ( dateMInput.value+ " / " + e.target.value);
+});
+cvcInput.addEventListener('change', (e) => {
+    cvc.textContent = e.target.value;
+});
+
